5bdde1d280b4d9f43d4ec391ea81f8a237819ea6
Chris Conti <starkiller515@fullsail.edu>
2013-03-14 19:56:26 -0400

    Project 2 3rd Change set
    Complete through Number Object

--- sdi-project2/Conti_Chris_Assignment2.js
+++ sdi-project2/Conti_Chris_Assignment2.js
@@ -26,23 +26,47 @@ var zombieHunt = function (numberZombies, numberDoors) {
 
     } else {
         console.log (survivorName[1] + "scouts from the tower.");
-    } ;
+    }
 
 };
     zombieHunt(10, 5);
 
 //Boolean
 
-var ammoRun = function(numRifleShells, numPistolRounds) {
+var getAmmoRun = function(numRifleShells, numPistolRounds) {
 
     if (numRifleShells && numPistolRounds < 100) {
-        console.log (survivorName[1] + " decides to make an ammo run.'");
+        console.log (survivorName[1] + " decides to make an ammo run.");
 
     }  else {
         console.log ("They have decided they have enough ammo for now.");
 
-    };
+    }
+    return getAmmoRun
 };
 
-    ammoRun()
+      getAmmoRun(numRifleShells, numPistolRounds)
+
+//Number
+
+console.log (survivorName[1] + " and " + survivorName[2] + " go to the " + safeHouse + " gate and start to countdown to when it is safe for " + survivorName[1] + " to run out.");
+console.log (survivorName[2] + " looks at " + survivorName[1] + " and says \"I will count down from 3, then run!\"");
+var count = 3;
+
+while (count > 0){
+
+    console.log (count + "!");
+
+    count = count - 1;
+
+    if (count > 0){
+        console.log (count + "!");
+    } else {
+        console.log ("GO!");
+
+    }
+};
+
+console.log (survivorName[1] + " runs out the gate, dodging walkers as he heads away from the " + safeHouse + ".");
+
 
