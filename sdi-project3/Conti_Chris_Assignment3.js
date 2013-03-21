dfb2658f01607d6c36d64ef908d78c9f0f22a95b
Chris Conti <starkiller515@fullsail.edu>
2013-03-21 11:32:47 -0400

    Project 3 commit 2
    Code from project 2 has been copied in to be refactored

--- sdi-project3/Conti_Chris_Assignment3.js
+++ sdi-project3/Conti_Chris_Assignment3.js
@@ -31,3 +31,76 @@ var truck = {
         this.flatTires = noFlats;
     }
 }; //end Object
+
+
+//Global variables
+var safeHouse = "prison";
+var numPistolRounds = 40;
+var numRifleShells = 50;
+var survivorName = ["Rick", "Daryl", "Shane"];
+
+
+//Procedure
+var zombieHunt = function (numberZombies, numberDoors) {
+
+    if (numberZombies > numberDoors) {
+        console.log (survivorName[0] + " goes to start clearing the walkers out.");
+
+    } else {
+        console.log (survivorName[1] + "scouts from the tower.");
+    }
+
+};
+    zombieHunt(10, 5); //CALL THIS FUNCTION
+
+//Boolean
+
+var getAmmoRun = function(numRifleShells, numPistolRounds) {
+
+    if (numRifleShells && numPistolRounds < 100) {
+        console.log (survivorName[1] + " decides to make an ammo run.");
+
+    }  else {
+        console.log ("They have decided they have enough ammo for now.");
+
+    }
+    return getAmmoRun;
+};
+
+        getAmmoRun(numRifleShells, numPistolRounds) //CALL THIS FUNCTION
+
+
+ // Number
+
+var readyGo = function(count) {
+
+    while (count > 0){
+
+        console.log (count + "!");
+
+        count = count -1;
+
+        if (count > 0){
+            console.log (count + "!");
+        } else {
+            console.log ("GO!");
+        };
+    }
+    return count;
+};
+
+ readyGo(3); //CALL THIS FUNCTION
+
+
+// String
+
+var climbTower = function() {
+
+    var openDoor = "kicks";
+    var hurryUp = "race";
+
+    return climbTower;
+
+};
+
+    climbTower(); // CALL THIS FUNCTION
