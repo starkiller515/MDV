585e8dc90ef8a3ddfa5bbf9786159bd09aeee77c
Chris Conti <starkiller515@fullsail.edu>
2013-03-21 11:13:02 -0400

    Project 3 commit 1
    json and object complete (accessor and mutator)

--- sdi-project3/Conti_Chris_Assignment3.js
+++ sdi-project3/Conti_Chris_Assignment3.js
@@ -1,1 +1,33 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+/* Chris Conti
+    Project 3
+    SDI 1303
+    Flow Chart Building and Objects
+    */
+
+//json data
+
+var survivors = [
+
+    { "name": "Rick", "weapon": "Colt Python", "job": "cop"},
+    { "name": "Daryl", "weapon": "crossbow", "job": "hunter"},
+    { "name": "Shane", "weapon": "shotgun", "job": "cop"}
+];
+
+//object
+
+var truck = {
+
+    "make" : "Ford",
+    "style" : "pickup",
+    "totalTires" : 4,
+    "flatTires" : 1,
+    "wheels" : function (){ //Method: Accessor
+        var goodWheels = this.totalTires - this.flatTires;
+        return goodWheels;
+    }, //end Method
+    "repairTires" : function(noFlats){ //Method: Mutator
+        this.flatTires = noFlats;
+    }
+}; //end Object
