d86b57318ca59240a26f541f9f8f0ca17cce377a
Chris Conti <starkiller515@fullsail.edu>
2013-03-28 20:30:58 -0400

    Project 4 Dry
    Initial project files

---
+++ sdi-project4/rename-me.js
@@ -0,0 +1,42 @@
+//alert("JavaScript works!");
+
+/*Chris Conti
+SDI 1303
+Project 4
+Code Library
+ */
+
+
+
+
+
+
+var myLibrary = function(){
+
+    //Problem 1
+    //Check to see if a string follows number pattern
+    var checkNumber = function () {
+
+       if
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    return {
+
+
+
+    }
+
+
+}
