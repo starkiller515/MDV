fc51a7cd8f906b49d086e1c4ba350255365f4e86
Chris Conti <starkiller515@fullsail.edu>
2013-05-09 13:25:44 -0400

    Added Vfw project 4

---
+++ MiU/Project 1/Conti_Chris_Project 4/js/json.js
@@ -0,0 +1,28 @@
+/**
+Chris Conti
+ VFW 1304
+ Project 4 Json Dummy Data File
+ */
+
+
+var json = {
+
+    "book1": {
+         "title":["Book Title", "Batman"],
+         "date": ["Date Added:", "05/15/2013"],
+        "booknumber":["Issue Number", "1"],
+        "coverstyle": ["Cover Style", "Variant"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "Very good"],
+        "grade": ["CCG Grading", "10"]
+    },
+    "book2": {
+        "title":["Book Title", "Nightwing"],
+        "date": ["Date Added:", "05/15/2013"],
+        "booknumber":["Issue Number", "4"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "ok"],
+        "grade": ["CCG Grading", "6"]
+    }
+}
