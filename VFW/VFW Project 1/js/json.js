52333b980edf586d6fd05c71e92617ac57e29b28
Chris Conti <starkiller515@fullsail.edu>
2013-05-09 22:50:43 -0400

    MiU Project 1
    Final files for MiU Project 1

---
+++ VFW/VFW Project 1/js/json.js
@@ -0,0 +1,190 @@
+/**
+Chris Conti
+ MiU 1305
+  Json Dummy Data File
+ */
+
+
+var json = {
+
+    "book1": {
+         "title":["Book Title", "Batman"],
+         "date": ["Date Added:", "05/15/2013"],
+        "booknumber":["Issue Number", "1"],
+        "coverstyle": ["Cover Style", "Variant"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "Very good"],
+        "grade": ["CCG Grading", "10"]
+    },
+    "book2": {
+        "title":["Book Title", "Nightwing"],
+        "date": ["Date Added:", "05/15/2013"],
+        "booknumber":["Issue Number", "4"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "ok"],
+        "grade": ["CCG Grading", "6"]
+    },
+    "book3": {
+        "title":["Book Title", "Catwoman"],
+        "date": ["Date Added:", "05/15/2013"],
+        "booknumber":["Issue Number", "3"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "ok"],
+        "grade": ["CCG Grading", "6"]
+    },
+    "book4": {
+        "title":["Book Title", "Catwoman"],
+        "date": ["Date Added:", "01/09/2013"],
+        "booknumber":["Issue Number", "8"],
+        "coverstyle": ["Cover Style", "Variant"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "new book"],
+        "grade": ["CCG Grading", "6"]
+    },
+    "book5": {
+        "title":["Book Title", "Batman"],
+        "date": ["Date Added:", "02/07/2013"],
+        "booknumber":["Issue Number", "2"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "Great"],
+        "grade": ["CCG Grading", "8"]
+    },
+    "book6": {
+        "title":["Book Title", "Nightwing"],
+        "date": ["Date Added:", "07/15/2011"],
+        "booknumber":["Issue Number", "24"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "ok"],
+        "grade": ["CCG Grading", "8"]
+    },
+    "book7": {
+        "title":["Book Title", "Nightwing"],
+        "date": ["Date Added:", "07/15/2012"],
+        "booknumber":["Issue Number", "98"],
+        "coverstyle": ["Cover Style", "Variant"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "ok"],
+        "grade": ["CCG Grading", "6"]
+    },
+    "book8": {
+        "title":["Book Title", "Batman"],
+        "date": ["Date Added:", "08/15/2013"],
+        "booknumber":["Issue Number", "9"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "No"],
+        "notes": ["Notes", "Nothing yet"],
+        "grade": ["CCG Grading", "6"]
+    },
+    "book9": {
+        "title":["Book Title", "Nightwing"],
+        "date": ["Date Added:", "05/12/2011"],
+        "booknumber":["Issue Number", "89"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "meh"],
+        "grade": ["CCG Grading", "7"]
+    },
+    "book10": {
+        "title":["Book Title", "Batman and Robin"],
+        "date": ["Date Added:", "01/19/2013"],
+        "booknumber":["Issue Number", "76"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "forgettable"],
+        "grade": ["CCG Grading", "4"]
+    },
+    "book11": {
+        "title":["Book Title", "Batman"],
+        "date": ["Date Added:", "02/15/2013"],
+        "booknumber":["Issue Number", "24"],
+        "coverstyle": ["Cover Style", "Sketch Variant"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "awesome art"],
+        "grade": ["CCG Grading", "7"]
+    },
+    "book12": {
+        "title":["Book Title", "Catwoman"],
+        "date": ["Date Added:", "05/15/2013"],
+        "booknumber":["Issue Number", "15"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "No"],
+        "notes": ["Notes", "Not yet read"],
+        "grade": ["CCG Grading", "9"]
+    },
+    "book13": {
+        "title":["Book Title", "Dark Knight"],
+        "date": ["Date Added:", "03/12/2013"],
+        "booknumber":["Issue Number", "5.7"],
+        "coverstyle": ["Cover Style", "Variant"],
+        "read": ["Read", "No"],
+        "notes": ["Notes", "none"],
+        "grade": ["CCG Grading", "6"]
+    },
+    "book14": {
+        "title":["Book Title", "Nightwing"],
+        "date": ["Date Added:", "05/6/2013"],
+        "booknumber":["Issue Number", "3"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "No"],
+        "notes": ["Notes", "what?!?"],
+        "grade": ["CCG Grading", "8.7"]
+    },
+    "book15": {
+        "title":["Book Title", "Detective Comics"],
+        "date": ["Date Added:", "05/15/2013"],
+        "booknumber":["Issue Number", "1"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "awesome"],
+        "grade": ["CCG Grading", "9.8"]
+    },
+    "book16": {
+        "title":["Book Title", "Nightwing"],
+        "date": ["Date Added:", "05/7/2013"],
+        "booknumber":["Issue Number", "8"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "No"],
+        "notes": ["Notes", "test"],
+        "grade": ["CCG Grading", "6"]
+    },
+    "book17": {
+        "title":["Book Title", "Batman"],
+        "date": ["Date Added:", "05/1/2013"],
+        "booknumber":["Issue Number", "9"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "blah"],
+        "grade": ["CCG Grading", "3.3"]
+    },
+    "book18": {
+        "title":["Book Title", "Catwoman"],
+        "date": ["Date Added:", "06/2/2012"],
+        "booknumber":["Issue Number", "6"],
+        "coverstyle": ["Cover Style", "Sketch Variant"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "New"],
+        "grade": ["CCG Grading", "9"]
+    },
+    "book19": {
+        "title":["Book Title", "Batman and Robin"],
+        "date": ["Date Added:", "05/12/2011"],
+        "booknumber":["Issue Number", "54"],
+        "coverstyle": ["Cover Style", "Variant"],
+        "read": ["Read", "Yes"],
+        "notes": ["Notes", "great"],
+        "grade": ["CCG Grading", "1"]
+    },
+    "book20": {
+        "title":["Book Title", "Nightwing"],
+        "date": ["Date Added:", "05/3/2013"],
+        "booknumber":["Issue Number", "12"],
+        "coverstyle": ["Cover Style", "Standard"],
+        "read": ["Read", "No"],
+        "notes": ["Notes", "could do without"],
+        "grade": ["CCG Grading", "3"]
+    }
+}
